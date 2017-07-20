47d5cdff0220abd47a37c797d21ebfbb9303bda5
Fabius Bile <dock234@yandex.ru>
2017-07-21 01:07:56 +0500

    Исправлена рассинхронизация анимаций атаки

--- Assets/Scripts/Core/Animated.cs
+++ Assets/Scripts/Core/Animated.cs
@@ -1,16 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Text.RegularExpressions;
+using UnityEditor;
+
+
 public class Animated : MonoBehaviour {
-	public string spriteSheet;
+	string spritePath;
 	Sprite[] sprites;
 	SpriteRenderer sRenderer;
-	public bool isWeapon;
-	public bool isClothe;
+	public Texture2D spriteSheet;
 	PlayerController player;
 
 	void Awake(){
-		sprites = Resources.LoadAll<Sprite>(spriteSheet);
+		spritePath = AssetDatabase.GetAssetPath (spriteSheet);
+		spritePath = spritePath.Replace (".png", "");
+		spritePath = spritePath.Replace ("Assets/Resources/", "");
+		sprites = Resources.LoadAll<Sprite>(spritePath);
+
 		sRenderer = GetComponent<SpriteRenderer> ();
 		player = transform.parent.parent.GetComponent<PlayerController> ();
 	}
@@ -20,17 +26,19 @@ public class Animated : MonoBehaviour {
 		// Так как для всех спрайтов персонажей и экипировки используются атласы с одинаковым расположением спрайтов
 		// вместо необходимости заново настраивать анимацию для каждого предмета во время игры выбираются спрайты с тем же номером, что и у
 		// тела персонажа
+		if (sprites.Length > 0) {
+			string baseSpriteName = sRenderer.sprite.name;
 
-		string baseSpriteName = sRenderer.sprite.name;
+			// Отделение номера спрайта от его имени
 
-		// Отделение номера спрайта от его имени
+			string spriteName = Regex.Match (baseSpriteName, @"\d*$").Value;
 
-		string spriteName = Regex.Match (baseSpriteName, @"\d*$").Value;
+			//Выбор из атласа спрайта с тем же номером, что и у тела в данный кадр анимации 
 
-		//Выбор из атласа спрайта с тем же номером, что и у тела в данный кадр анимации 
+			Sprite newSprite = sprites [int.Parse (spriteName)];
+			sRenderer.sprite = newSprite;
+		}
 
-		Sprite newSprite = sprites [int.Parse (spriteName)];
-		sRenderer.sprite = newSprite;
 	}
 	public void StopAttack(){
 		player.StopAttack ();
